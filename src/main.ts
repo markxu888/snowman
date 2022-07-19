792a63aec4eca3b087eedbc4e3cd9ef2b85555e9
markxu888 <1119026220@qq.com>
2022-07-19 12:58:49 +0800

    main

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 createApp(App).use(store).use(router).mount('#app')
+
+App.config.globalProperties.$axiso = axios
+// const app = createApp(App)
+// app.use(store)
+// app.use(ElementPlus)
+// app.use(router)
+// app.mount('#app')
