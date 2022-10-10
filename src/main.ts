caa49fb18bb62c3c30e99078e7e1fe46b26e8b8b
markxu888 <1119026220@qq.com>
2022-10-10 10:24:39 +0800

    1010

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+// import './registerServiceWorker'页面缓存
 import router from './router'
 import store from './store'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import axios from 'axios'
 
 createApp(App).use(store).use(router).mount('#app')
